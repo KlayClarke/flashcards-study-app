2fed0f55c0d18fbcbd97a7a060a1f8f92472c517
Klay Clarke <klayaclarke@gmail.com>
2021-11-25 10:57:06 -0500

    create method that shows front of flashcard (along with random French word from csv)

--- main.py
+++ main.py
@@ -1,31 +1,51 @@
+import csv
+import random
 from tkinter import *
 
 BACKGROUND_COLOR = "#B1DDC6"
-
-
-# ------------------ BUTTONS FUNCTIONALITY --------------- #
-# ----------RIGHT BUTTON ------------#
-def right():
-    print('correct')
-
-
-# ----------WRONG BUTTON-------------#
-def wrong():
-    print('incorrect')
-
 
 # ------------------ UI -------------------- #
 window = Tk()
 window.title('Flashcards')
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
-# ------------- CARD FRONT and BACK --------- #
 canvas = Canvas(width=900, height=600, bg=BACKGROUND_COLOR, highlightthickness=0)
 card_front = PhotoImage(file='images/card_front.png')
 canvas.create_image(450, 300, image=card_front)
 title_text = canvas.create_text(450, 150, text='Title', font=('Arial', 40, 'italic'))
 word_text = canvas.create_text(450, 300, text='Word', font=('Arial', 60, 'bold'))
 canvas.grid(column=0, row=0, columnspan=2)
+
+# --------------- CHANGE CARD --------------------------- #
+with open('data/french_words.csv') as file:
+    reader = csv.reader(file)
+    data = list(reader)
+french_text_display = data[0][0]
+english_text_display = data[0][1]
+
+
+def card():
+    random_num = random.randint(1, 102)
+    random_word_coupling = data[random_num]
+    random_french_word = random_word_coupling[0]
+    random_english_translation = random_word_coupling[1]
+    canvas.itemconfig(title_text, text=french_text_display)
+    canvas.itemconfig(word_text, text=random_french_word)
+    # print(random_word_coupling)
+    # print(f'{french_text_display}: {random_french_word}')
+    # print(f'{english_text_display}: {random_english_translation}')
+
+
+card()
+
+
+# ------------------ BUTTONS FUNCTIONALITY --------------- #
+def right():
+    print('correct')
+
+
+def wrong():
+    print('incorrect')
 
 
 # ------- BUTTONS ---------- #
