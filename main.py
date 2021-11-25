bb784e296dc352c33c9d635edeb2750488e2f558
Klay Clarke <klayaclarke@gmail.com>
2021-11-25 08:27:24 -0500

    create application window and 'right' and 'wrong' buttons

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
+from tkinter import *
+
 BACKGROUND_COLOR = "#B1DDC6"
 
+# ------------------ UI -------------------- #
+window = Tk()
+window.title('Flashcards')
+
+# ------------- CARD FRONT and BACK --------- #
+
+
+# ------- BUTTONS ---------- #
+right_image = PhotoImage(file='images/right.png')
+right_button = Button(image=right_image, highlightthickness=0)
+
+wrong_image = PhotoImage(file='images/wrong.png')
+wrong_button = Button(image=wrong_image, highlightthickness=0)
+
+window.mainloop()
