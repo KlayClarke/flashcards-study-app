1e90307682e8066e2acd38efdd9246998888e127
Klay Clarke <klayaclarke@gmail.com>
2021-11-25 09:03:00 -0500

    create canvas to house flashcards

--- main.py
+++ main.py
@@ -2,18 +2,36 @@ from tkinter import *
 
 BACKGROUND_COLOR = "#B1DDC6"
 
+
+# ------------------ BUTTONS FUNCTIONALITY --------------- #
+# ----------RIGHT BUTTON ------------#
+def right():
+    print('correct')
+
+
+# ----------WRONG BUTTON-------------#
+def wrong():
+    print('incorrect')
+
+
 # ------------------ UI -------------------- #
 window = Tk()
 window.title('Flashcards')
+window.config(padx=50, pady=50)
 
 # ------------- CARD FRONT and BACK --------- #
-
+canvas = Canvas(width=900, height=600)
+card_front = PhotoImage(file='images/card_front.png')
+canvas.create_image(450, 300, image=card_front)
+canvas.grid(column=0, row=0, columnspan=2)
 
 # ------- BUTTONS ---------- #
-right_image = PhotoImage(file='images/right.png')
-right_button = Button(image=right_image, highlightthickness=0)
-
 wrong_image = PhotoImage(file='images/wrong.png')
-wrong_button = Button(image=wrong_image, highlightthickness=0)
+wrong_button = Button(image=wrong_image, highlightthickness=0, command=wrong)
+wrong_button.grid(column=0, row=1)
+
+right_image = PhotoImage(file='images/right.png')
+right_button = Button(image=right_image, highlightthickness=0, command=right)
+right_button.grid(column=1, row=1)
 
 window.mainloop()
